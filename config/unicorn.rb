6fe2e31254a3992bbf78b90583a426921b82280a
Artem Bubnov <tyomik16@rambler.ru>
2013-09-19 12:57:34 +0400

    add unicorn files

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,9 @@
-root = "/home/deployer/apps/akcent/current"
+root = "/home/deployer/apps/visitka/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/tmp/unicorn.akcent.sock"
+listen "/tmp/unicorn.visitka.sock"
 worker_processes 2
 timeout 30
