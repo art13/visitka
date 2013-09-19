f02d8780dfd67a1a81c9871eda6e64be44b09f3b
Artem Bubnov <tyomik16@rambler.ru>
2013-09-19 16:56:55 +0400

    add books view

--- app/models/material.rb
+++ app/models/material.rb
@@ -2,5 +2,5 @@ class Material < ActiveRecord::Base
 	has_many :releases
 	has_many :programs_features
 	accepts_nested_attributes_for :releases, :allow_destroy =>true
-	attr_accessible :name, :description, :releases, :created_at, :updated_at, :release,:releases_attributes
+	attr_accessible :name, :description,:preview, :releases, :created_at, :updated_at, :release,:releases_attributes
 end
