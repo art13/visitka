2bb3ac192b7b3372e8173d3d6bdbf95c82b766fb
Artem Bubnov <tyomik16@rambler.ru>
2013-09-20 11:46:23 +0400

    upd verstka

--- app/assets/javascripts/spy.js
+++ app/assets/javascripts/spy.js
@@ -1,2 +1,7 @@
+$('document').ready(function(){
+	var body=$('body').attr('class');
+	$('.menus').children().removeClass('active')
+	$('.menus').children().filter('#'+body).addClass('active')
 
+});
 
