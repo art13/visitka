61621bd115300bbb7f2a50e6fee5ec951c9c5c65
Artem Bubnov <tyomik16@rambler.ru>
2013-12-10 14:32:06 +0400

    admin and lk pages  changes

--- app/admin/material.rb
+++ app/admin/material.rb
@@ -3,6 +3,7 @@ ActiveAdmin.register Material,  { :sort_order => :id_asc } do
 		filter :name, :label=> I18n.t('name_f')
  	scope :all, :default => true
 	config.per_page = 8
+	actions :all, :except => [:destroy]
  	index do
  		selectable_column
  		column  :name 
@@ -12,13 +13,19 @@ ActiveAdmin.register Material,  { :sort_order => :id_asc } do
  		column 	:release_file_name
  		column  :created_at
  		column  :updated_at
-  		default_actions
+  		column '' do |resource|
+	 		 links = ''.html_safe
+	 		 links += link_to I18n.t('active_admin.view')+' ', resource_path(resource)
+	 		 links += link_to I18n.t('active_admin.edit'), edit_resource_path(resource)
+	  		# links += link_to ' '+I18n.t('active_admin.delete'), resource_path(resource), :method => :delete, :data=>{:confirm =>I18n.t('destroy_alert')}
+      		 links
+		end
  	end
  	form html: { multipart: true }do |f|
  		f.inputs t('materials') do
  			f.input :name
  			f.input :installer_version
- 			f.input :installator, as: :select , :collection => Installator.all.map { |a| [ a.name+' v-'+ a.version]}
+ 			f.input :installator, as: :select , :collection => Installator.all.map { |a| [ a.name+' v-'+ a.version, a.id]}
  			f.input :preview, :hint=>I18n.t('max_client_comment')
  			f.input :version, as: :radio, :collection => [t('demo'),t('full')]
 			f.input :description,  as: :wysihtml5
