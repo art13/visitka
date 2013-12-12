1fda47dfe9aa7403d34fea56283bf2bb5f6bb2f4
Artem Bubnov <tyomik16@rambler.ru>
2013-12-12 13:58:52 +0400

    add setup mail

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => 'loft-leshoz.ru',
-  #:user_name            =>  DataContact.first.deliver_email.split('@').first,
-  #:password             =>  DataContact.first.deliver_pass,
+  :user_name            =>  DataContact.first.deliver_email.split('@').first,
+  :password             =>  DataContact.first.deliver_pass,
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
