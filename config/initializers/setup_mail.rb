924e37274805157c31872a7b7e84867c486ba01b
Bubnov <bubnov.art@gmail.com>
2016-07-29 16:06:46 +0300

    some changes

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,11 +1,11 @@
-ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => 'loft-leshoz.ru',
-  :user_name            =>  DataContact.first.deliver_email.split('@').first,
-  :password             =>  DataContact.first.deliver_pass,
-  :authentication       => "plain",
-  :enable_starttls_auto => true
-}
-ActionMailer::Base.default_url_options[:host] = "loft-leshoz.ru"
+# ActionMailer::Base.smtp_settings = {
+#   :address              => "smtp.gmail.com",
+#   :port                 => 587,
+#   :domain               => 'loft-leshoz.ru',
+#   :user_name            =>  DataContact.first.deliver_email.split('@').first,
+#   :password             =>  DataContact.first.deliver_pass,
+#   :authentication       => "plain",
+#   :enable_starttls_auto => true
+# }
+# ActionMailer::Base.default_url_options[:host] = "loft-leshoz.ru"
 #Mail.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
